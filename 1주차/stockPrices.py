6d7444c5a7f87e7a9ff3725b2dd597ea967cdfd2
kwanghwi <nimo6289@gmail.com>
2024-07-14 15:45:39 +0900

    programmers 주식가격

--- 1주차/stockPrices.py
+++ 1주차/stockPrices.py
@@ -1,14 +1,14 @@
 def solution(prices):
     lst = []
     for i in range(len(prices)):
-        ans = 0
+        time = 0
         for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                ans += 1
-            else:
-                ans += 1
+            time += 1
+            if prices[i] > prices[j]:
                 break
-        lst.append(ans)
+        lst.append(time)
     return lst
 
 print(solution([1, 2, 3, 2, 3]))
+
+
