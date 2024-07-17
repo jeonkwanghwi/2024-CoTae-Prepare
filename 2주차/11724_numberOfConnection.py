39ded50a6a47c514f47d7451a1544db62078f318
kwanghwi <nimo6289@gmail.com>
2024-07-18 00:25:09 +0900

    11724 연결 요소의 개수

--- 2주차/11724_numberOfConnection.py
+++ 2주차/11724_numberOfConnection.py
@@ -1,26 +1,27 @@
 import sys
-imput = sys.stdin.readline
-sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+sys.setrecursionlimit(5000)
 
 n, m = map(int, input().split()) # 정점개수 n , 간선개수 m
-graph = [[False] * (n + 1) for _ in range(n + 1)] # 그래프 선언
+graph = [[] for _ in range(n+1)]
 visited = [False] * (n + 1)
 count = 0 # 연결 요소 개수
 
+# 연결 리스트로 초기화하는 방법
 for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-def DFS(v):
+def dfs(v):
     visited[v] = True
-    for i in range(1, n+1):
-        if visited[i] == False:
-            DFS(i)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
 
 for i in range(1, n+1):
-    if visited[i] == False:
-        DFS(i)
+    if not visited[i]:
         count += 1
+        dfs(i)
 
 print(count)
