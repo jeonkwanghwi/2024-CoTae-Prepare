9528778600ace088a4876dc119ad5deaf67fea70
kwanghwi <nimo6289@gmail.com>
2024-07-18 21:36:54 +0900

    1012 유기농 배추

--- 2주차/1260_DFS_BFS.py
+++ 2주차/1260_DFS_BFS.py
@@ -14,12 +14,21 @@ visited1 = [False] * (n + 1)  # dfs 방문기록
 visited2 = [False] * (n + 1)  # BFS 방문기록
 ############################################## 초기세팅 끝
 
-def DFS(v):
-    visited1[v] = True # 현재 노드는 방문한것
-    print(v, end=' ') # 방문한 노드 출력
-    for i in range(1, n+1):
-        if visited1[i] == False and graph[v][i] == 1: # 해당 노드를 아직 방문하지 않았고, 노드 i와 연결되어 있는 경우
-            DFS(i) # 방문하지 않은 노드 계속 방문
+def DFS(graph, x, y):
+    # 오류가 나는 상황을 정리한 것. 위, 아래로 움직이다가 x, y의 범위 밖으로 나가는 경우를 의미함.
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+
+    if graph[x][y] == 1: # 1인 집은 탐색 대상임
+        global part_house # 구역별 집 개수를 의미함
+        part_house += 1
+        graph[x][y] = 0 # 탐색 후, 탐색 완료의 의미로 0으로 바꿈
+        for i in range(4): # 상하좌우 4개의 방향을 체크함
+            nx = x + dx[i] # 기존의 좌표에 상하좌우를 더해줌
+            ny = y + dy[i]
+            DFS(graph, nx, ny) # 더해준 상하좌우로 다시 탐색
+        return True # 모든 탐색이 완료됨
+    return False # 탐색이 끝나긴 했는데 0을 만나서 끝남. 즉 1로 둘러싼 경계 밖을 간 것.
 
 def BFS(v):
     queue = deque([v])
